90345696a7a6dab8c41ff1bf2f1fcccac6f32a23
Everton Carvalho <evertooon03@gmail.com>
2022-03-22 09:58:15 -0300

    Classe Personagem e Jogo concluidas e TESTADAS

---
+++ aula01_dotnet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace aula01_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Olá ");
+            Console.Write("mundo!");
+        }
+    }
+}
---
+++ exec3_5.cs
@@ -0,0 +1,10 @@
+public class exec3_5
+{
+    int numero1, numero2;
+    int Soma(int x, int y)
+    {
+        return x + y;
+    }
+    Console.log("SOMA \n %i", Soma(2,6));
+}
+
